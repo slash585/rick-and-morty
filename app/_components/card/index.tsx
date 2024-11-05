ca4bd4c18d33ca966c9a1382f56b6a76c8ba8f98
slash585 <slsh585@gmail.com>
2024-11-05 22:14:31 +0300

    Add navigation link to card component

--- app/_components/card/index.tsx
+++ app/_components/card/index.tsx
@@ -1,6 +1,7 @@
 import { Character } from "@/app/types/character";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CardProps {
   character: Character;
@@ -8,18 +9,20 @@ interface CardProps {
 
 function Card({ character }: CardProps) {
   return (
-    <div key={character.id} className="p-4 border rounded shadow">
-      <Image
-        src={character.image}  
-        alt={character.name}    
-        width={200}             
-        height={200}            
-        className="rounded-md" 
-      />
-      <h2 className="text-lg font-semibold">{character.name}</h2>
-      <p>Status: {character.status}</p>
-      <p>Gender: {character.gender}</p>
-    </div>
+    <Link href={`details/${character.id}`} passHref>
+      <div key={character.id} className="p-4 border rounded shadow">
+        <Image
+          src={character.image}
+          alt={character.name}
+          width={200}
+          height={200}
+          className="rounded-md"
+        />
+        <h2 className="text-lg font-semibold">{character.name}</h2>
+        <p>Status: {character.status}</p>
+        <p>Gender: {character.gender}</p>
+      </div>
+    </Link>
   );
 }
 
