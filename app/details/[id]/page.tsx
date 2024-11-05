8acd431872e7476ca878ab986116335ef7dc6800
slash585 <slsh585@gmail.com>
2024-11-05 22:20:11 +0300

    Implement generate metadata

--- app/details/[id]/page.tsx
+++ app/details/[id]/page.tsx
@@ -1,8 +1,19 @@
 import { Character } from "@/app/types/character"
+import { Metadata } from "next" 
 
 async function fetchCharacterDetails(id: string) {
   let data = await fetch(`${process.env.RICK_AND_MORTY_BASE_URL}/character/${id}`)
   return data.json()
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const { id } = await params
+  const character: Character = await fetchCharacterDetails(id);
+  
+  return {
+    title: character.name,
+    description: `Details about ${character.name} from Rick and Morty`
+  };
 }
 
 async function Details({ params }: { params: { id: string } }) {
