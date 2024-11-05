bbcd8075269078e5a136abde504bc4e6a0d1f6be
slash585 <slsh585@gmail.com>
2024-11-05 21:33:58 +0300

    Fetch characters

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,25 @@
-export default function Home() {
+import { Characters } from "./types/character"; 
+
+async function fetchCharacters() {
+  let data = await fetch(`${process.env.RICK_AND_MORTY_BASE_URL}/character`)
+  return data.json()
+}
+
+export default async function Home() {
+
+  let characters: Characters | undefined
+  let error = null
+
+  try {
+    characters = await fetchCharacters()
+  } catch(err) {
+    error = "Something went wrong"
+  }
+
+  if (error) {
+    return <div>{error}</div>
+  } 
+  
   return (
     <div className="">
 
