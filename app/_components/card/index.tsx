3cab3df14a0764cd76e1faf3be13051d0683a4bf
slash585 <slsh585@gmail.com>
2024-11-05 22:54:01 +0300

    Edit css in character list component

--- app/_components/card/index.tsx
+++ app/_components/card/index.tsx
@@ -10,17 +10,17 @@ interface CardProps {
 function Card({ character }: CardProps) {
   return (
     <Link href={`details/${character.id}`} passHref>
-      <div key={character.id} className="p-4 border rounded shadow">
+      <div key={character.id} className="p-4 border rounded shadow border-[#97ce4c] flex flex-col justify-center align-middle text-center">
         <Image
           src={character.image}
           alt={character.name}
           width={200}
           height={200}
-          className="rounded-md"
+          className="rounded-md m-auto"
         />
-        <h2 className="text-lg font-semibold">{character.name}</h2>
-        <p>Status: {character.status}</p>
-        <p>Gender: {character.gender}</p>
+        <h2 className="text-lg font-semibold mt-2 hover:text-[#97ce4c]">{character.name}</h2>
+        <p className="hover:text-[#97ce4c]">Status: {character.status}</p>
+        <p className="hover:text-[#97ce4c]">Gender: {character.gender}</p>
       </div>
     </Link>
   );
